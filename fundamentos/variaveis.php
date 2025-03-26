ddaee6992b2dc35135941985c3e8f83a68c4b4ef
WESLEY ANJOS <wesleyanjosfullstack@gmail.com>
2025-03-26 10:38:32 -0300

    adicioando exemplo em variáveis

--- fundamentos/variaveis.php
+++ fundamentos/variaveis.php
@@ -3,4 +3,9 @@
 $name = 'Wesley Anjos';
 echo $name;
 
+$firstName = 'wesley';
+$lastName = 'anjos';
+echo '<br>'.$firstName. ' ' .$lastName. '<br>';
+echo '<br>'.ucfirst($firstName). ' ' .ucfirst($lastName). '<br>';
+
 ?>
